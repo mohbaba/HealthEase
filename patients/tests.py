4c6fc54d563e49e5b73718d4649d87bf7f67c71d
susannaholubiyi <susannaholubiyi@gmail.com>
2024-07-31 13:40:42 +0100

    Initial Commit

--- patients/tests.py
+++ patients/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from rest_framework.test import APIClient, APITestCase
+
 
 # Create your tests here.
+
+class TestPatients(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+
