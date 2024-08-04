61104e235c21aa7883a449d46c09570599eaf5d0
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2024-08-04 23:22:36 +0100

    labs serializer

---
+++ labs/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Lab
+
+
+class LabSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lab
+        fields = [
+            'id',
+            'name',
+            'user_profile',
+            'location',
+            'description',
+            'contact_phone',
+            'contact_email',
+            'opening_hours',
+            'equipment_list',
+            'capacity',
+            'date_established'
+        ]
