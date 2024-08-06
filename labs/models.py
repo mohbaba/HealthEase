ad968f26bdb8d2cf065d2e92ad14ffcfc57563de
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2024-08-06 11:50:43 +0100

    pharmacy correction

--- labs/models.py
+++ labs/models.py
@@ -10,7 +10,6 @@ class Lab(models.Model):
     name = models.CharField(max_length=100, default=None)
     user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE, related_name='lab')
 
-
     class Meta:
         verbose_name = 'Lab'
         verbose_name_plural = 'Labs'
