db8b855e8a9dfd2764a28b69710b29642dfc021d
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2024-08-06 11:46:14 +0100

    pharmacy correction

--- labs/models.py
+++ labs/models.py
@@ -9,12 +9,7 @@ from users.models import UserProfile
 class Lab(models.Model):
     name = models.CharField(max_length=100, default=None)
     user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE, related_name='lab')
-    location = models.CharField(max_length=255, blank=True, null=True)
-    established_date = models.DateField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    contact_number = models.CharField(max_length=15, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    is_active = models.BooleanField(default=True)
+
 
     class Meta:
         verbose_name = 'Lab'
