4891a8e61b75773c1d0d99bea5ab784d43abf066
Muhammad Baba Muhammad <59345145+mohbaba@users.noreply.github.com>
2024-08-21 05:06:38 +0100

    Added: Phone Number Validation

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.db.models import CASCADE
 
